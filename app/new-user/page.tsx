d908910cca62c8353e1d02ef12c84983b6e6a3cf
David Ramsdale <davidramsdale1991@gmail.com>
2024-04-23 14:53:52 +1000

    feat: add clerk and sign in and sign up pages

---
+++ app/new-user/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Welcome to my page.</p>
+    </div>
+  );
+};
+
+export default Page;
