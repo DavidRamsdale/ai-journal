5a8dc5e7e81a96980d2b0a09662f5053929e18d6
David Ramsdale <davidramsdale1991@gmail.com>
2024-04-23 15:36:23 +1000

    feat: add user journal entry and analysis model to schema

---
+++ prisma/migrations/20240423053508_002/migration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[clerkId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `clerkId` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "clerkId" STRING NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "User" ADD COLUMN     "email" STRING NOT NULL;
+ALTER TABLE "User" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "JournalEntry" (
+    "id" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" STRING NOT NULL,
+    "content" STRING NOT NULL,
+
+    CONSTRAINT "JournalEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Analysis" (
+    "id" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "entryId" STRING NOT NULL,
+    "mood" STRING NOT NULL,
+    "summary" STRING NOT NULL,
+    "color" STRING NOT NULL,
+    "negative" BOOL NOT NULL,
+
+    CONSTRAINT "Analysis_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "JournalEntry_userId_idx" ON "JournalEntry"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Analysis_entryId_key" ON "Analysis"("entryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
